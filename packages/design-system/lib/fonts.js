4a7798b1de14fde3f8a8fd45a93c987dab935633
Damir Akzhigitov <damir.cy@gmail.com>
2025-01-27 09:15:28 +0200

    chore: remove build only files

--- packages/design-system/lib/fonts.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.fonts = void 0;
-const utils_1 = require("@repo/design-system/lib/utils");
-const mono_1 = require("geist/font/mono");
-const sans_1 = require("geist/font/sans");
-exports.fonts = (0, utils_1.cn)(sans_1.GeistSans.variable, mono_1.GeistMono.variable, 'touch-manipulation font-sans antialiased');
--- packages/design-system/providers/theme.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ThemeProvider = void 0;
-const jsx_runtime_1 = require("react/jsx-runtime");
-const next_themes_1 = require("next-themes");
-const ThemeProvider = ({ children, ...properties }) => ((0, jsx_runtime_1.jsx)(next_themes_1.ThemeProvider, { attribute: "class", defaultTheme: "system", enableSystem: true, disableTransitionOnChange: true, ...properties, children: children }));
-exports.ThemeProvider = ThemeProvider;
